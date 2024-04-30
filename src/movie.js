c4f36833bd36d053220a138ac19dc07043f5c442
joymet33 <joymet33@gmail.com>
2024-04-30 17:19:31 +0900

    modified with feedback(2)

--- src/movie.js
+++ src/movie.js
@@ -12,3 +12,12 @@ cardList.innerHTML = moives
     )
 
     .join("");
+
+
+    // cardList.addEventListener("click", handleClickCard);
+
+    function handleClickCard ( 타겟 ) {
+        // 1. 카드 외 영역 클릭 시 - return (함수 적용x)
+        // 2. 카드 클릭 시 - id 알림창
+        // 3. 카드 안 요소(=자식 =img, h3, p) 클릭 시 - 카드(=부모) id 알림창
+    }
