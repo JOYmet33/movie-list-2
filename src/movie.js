c60209707083c173019d59d157288d04e71fbaf1
joymet33 <joymet33@gmail.com>
2024-05-02 11:20:15 +0900

    before testing 유효성

--- src/movie.js
+++ src/movie.js
@@ -1,6 +1,15 @@
 export const cardList = document.querySelector("#card-list");
+// export : ~~을 수출한다(=밖으로 내보내도록 승인한다)
+// const cardList : ~~을 cardList라는 변수로 선언한다
+// document : 전체 웹페이지
+// querySelector : CSS선택자(=Selector) 통해서 특정 요소(=괄호 안에 있는 요소)를 선택한다
+// "#card-list" : card-list라는 id를 가진 요소
+// ==> html 전체에서, CSS선택자를 통해 선택한, id "card-list"를, cardlist 라고 변수를 선언하고, 이 변수를 외부로 보내도록 승인함
 
 cardList.innerHTML = moives
+// cardList : html 전체 문서의 id "card-list"를 말함
+// innerHTML : ~을 html 문서에 삽입하겠다
+// moives : 아래 map함수의 결과값을 담을 그릇
     .map(
         (movie) => `
         <li class="movie-card" id=${movie.id}>
@@ -10,6 +19,7 @@ cardList.innerHTML = moives
         <p>평점: 9.1</p>
         </li>`
     )
+
 
     .join("");
 
